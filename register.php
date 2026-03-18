3a67d9ec9b54c3518266a18fadeae27a2005e678
Kayden <62318961+Marvoos@users.noreply.github.com>
2026-03-18 17:00:24 -0400

    forms completed

--- register.php
+++ register.php
@@ -12,6 +12,9 @@
     <meta name="description" content="A local library service to view and hold books">
     <!--Linking the external stylesheet-->
     <link rel="stylesheet" href="stylesheets/styles.css">
+
+    <link rel="stylesheet" href="stylesheets/formstyle.css">
+    <script src="https://kit.fontawesome.com/7d8aa418e1.js" crossorigin="anonymous"></script>
     
     <title>SLS</title>
 </head>
@@ -50,7 +53,21 @@
     </nav>
 
     <main class="content">
-
+        <div class="form">
+            <form method="POST">
+                <h2>Register</h2>
+                <input type="text" name="fname" placeholder="First Name" required>
+                <input type="text" name="lname" placeholder="Last Name" required>
+                <input type="email" name="email" autocomplete="email" placeholder="Email Address" required>
+                <div class="pass-div">
+                    <input type="password" name="password" placeholder="Password" required><span id="toggle-pass" class="fa-solid fa-eye"></span>
+                </div>
+                <div>
+                    <input type="submit" class="button">
+                    <p>Already have an account? <a href="signin.php">Sign in</a></p>
+                </div>
+            </form>
+        </div>
     </main>
 </body>
 </html>
--- signin.php
+++ signin.php
@@ -12,6 +12,9 @@
     <meta name="description" content="A local library service to view and hold books">
     <!--Linking the external stylesheet-->
     <link rel="stylesheet" href="stylesheets/styles.css">
+
+    <link rel="stylesheet" href="stylesheets/formstyle.css">
+    <script src="https://kit.fontawesome.com/7d8aa418e1.js" crossorigin="anonymous"></script>
     
     <title>SLS</title>
 </head>
@@ -50,7 +53,19 @@
     </nav>
 
     <main class="content">
-
+        <div class="form">
+            <form method="POST">
+                <h2>Sign In</h2>
+                <input type="email" name="email" autocomplete="email" placeholder="Email Address" required>
+                <div class="pass-div">
+                    <input type="password" name="password" placeholder="Password" required><span id="toggle-pass" class="fa-solid fa-eye"></span>
+                </div>
+                <div>
+                    <input type="submit" class="button">
+                    <p>Don't have an account? <a href="register.php">Register</a></p>
+                </div>
+            </form>
+        </div>
     </main>
 </body>
 </html>
